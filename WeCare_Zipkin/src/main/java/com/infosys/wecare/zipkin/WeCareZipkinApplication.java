b46e55b0f58f091f422cc5ec6e09d094504ba85e
Utkarsh Yadav <37462832+utkarshyadav46@users.noreply.github.com>
2021-01-12 22:14:08 +0530

    Add files via upload

---
+++ WeCare_Zipkin/src/main/java/com/infosys/wecare/zipkin/WeCareZipkinApplication.java
@@ -0,0 +1,17 @@
+package com.infosys.wecare.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import zipkin2.server.internal.EnableZipkinServer;
+
+
+
+@SpringBootApplication
+@EnableZipkinServer
+public class WeCareZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeCareZipkinApplication.class, args);
+	}
+}
